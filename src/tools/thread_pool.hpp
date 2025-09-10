c1e727e2dd15cf396752f88bbc1259bb4ff567ce
ranshu <ran.shu@i-tage.com>
2025-09-10 14:58:44 +0800

    Add thread pool tools

---
+++ src/tools/thread_pool.hpp
@@ -0,0 +1,117 @@
+#ifndef THREAD_POOL_HPP
+#define THREAD_POOL_HPP
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <future>
+
+namespace puppet_master
+{
+namespace tools
+{
+
+class ThreadPool 
+{
+public:
+    ThreadPool(size_t threads);
+    ~ThreadPool();
+
+    template<class F, class... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
+
+    void wait();
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    std::condition_variable finished_condition;
+    bool stop;
+    size_t active_tasks;
+};
+
+inline ThreadPool::ThreadPool(size_t threads)
+    : stop(false), active_tasks(0)
+{
+    for(size_t i = 0; i < threads; ++i)
+        workers.emplace_back(
+            [this]
+            {
+                for(;;)
+                {
+                    std::function<void()> task;
+
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        this->condition.wait(lock,
+                            [this]{ return this->stop || !this->tasks.empty(); });
+                        if(this->stop && this->tasks.empty())
+                            return;
+                        task = std::move(this->tasks.front());
+                        this->tasks.pop();
+                        ++active_tasks;
+                    }
+
+                    task();
+
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        --active_tasks;
+                        if (tasks.empty() && active_tasks == 0) {
+                            finished_condition.notify_all();
+                        }
+                    }
+                }
+            }
+        );
+}
+
+inline ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for(std::thread &worker: workers)
+        worker.join();
+}
+
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>
+{
+    using return_type = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+
+        if(stop)
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+
+        tasks.emplace([task](){ (*task)(); });
+    }
+    condition.notify_one();
+    return res;
+}
+
+inline void ThreadPool::wait()
+{
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    finished_condition.wait(lock, [this] { return tasks.empty() && active_tasks == 0; });
+}
+
+}
+}
+
+#endif
